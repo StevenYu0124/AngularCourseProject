71f125bc968106828b13cb25631d39c388348779
Steven Yu <e0390018@u.nus.edu>
2023-07-27 00:58:16 +0800

    follow course

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe(
+      "Chocolate",
+      "Damn sweet, damn good",
+      "https://www.livewellbakeoften.com/wp-content/uploads/2019/08/Fudge-Recipe-36-s-copy.jpg"
+    ),
+    new Recipe(
+      "Hungarian Chicken Paprikash",
+      "Curry❤️",
+      "https://unsophisticook.com/wp-content/uploads/2017/11/Hungarian-Chicken-Paprikash-Recipe-720x540.jpg"
+    )
+  ]
 }
